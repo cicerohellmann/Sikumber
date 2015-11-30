e5750789de5ed40259f157931ca8c07b8c5b0d2b
Cicero Jacobi <cicerohellmann@gmail.com>
2015-11-30 11:23:03 -0200

    fix on the test

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,19 +1,24 @@
-s = Sikulix::Screen.new
+ #
 Given(/^I call my app$/) do
-  appXXX = Sikulix::App("Calculator")
+  @s = Sikulix::Screen.new
+  firefox = "/Users/cicerojacobi/start_emulator.sh"
+  appXXX = Sikulix::App(firefox)
   appXXX.focus()
+  appXXX.window()
+
 end
 
 Then(/^I do a sum$/) do
-  s.click(Sikulix::Pattern.new("two_button.png").similar(0.0))
-  s.click("plus_button.png")
-  s.click("two_button.png")
-  s.click("equal_button.png")
+  # s.click(Sikulix::Pattern.new("two_button.png").similar(0.0))
+   @s.click("images_folder.png")
+  # s.click("plus_button.png")
+  # s.click("two_button.png")
+  # s.click("equal_button.png")
 end
 
 When(/^I see my result$/) do
-  s.exists("three.png")
-  s.click("three.png")
+  # s.exists("three.png")
+  # s.click("three.png")
 end
 
 
